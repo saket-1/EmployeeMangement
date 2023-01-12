9e1d948bc016a306e0f8e5cc74394a80e1322985
saket-1 <saket06205@gmail.com>
2023-01-12 15:17:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import AddEmployeeComponent from './components/AddEmployeeComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+      <HeaderComponent/>
+      <div className='container'>
+        <Routes>
+          <Route exact path = '/' element={<ListEmployeeComponent/>}/>
+          <Route exact path = '/employees' element={<ListEmployeeComponent/>}/>
+          <Route exact path = '/add-employee' element={<AddEmployeeComponent/>}/>
+          <Route exact path = '/edit-employee/:id' element = {<AddEmployeeComponent/>}/>
+        </Routes>
+     
+      </div>
+      <FooterComponent/>
+      </Router> 
     </div>
   );
 }
